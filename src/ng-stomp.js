48b6c65719ebe6320beb7d463431e02637c1425d
Wolfgang Jentner <wolfgang.jentner@uni-konstanz.de>
2017-07-27 20:57:34 +0200

    added jshint and resolved warnings

--- src/ng-stomp.js
+++ src/ng-stomp.js
@@ -6,26 +6,26 @@
  * @license MIT
  */
 
-/*global
-    angular, SockJS, Stomp */
+/*global angular, SockJS, Stomp, define */
 
 (function (factory) {
-  'use strict'
+  'use strict';
   if (typeof exports === 'object') {
     // Node/CommonJS
     module.exports = factory(
       typeof angular !== 'undefined' ? angular : require('angular'),
       typeof SockJS !== 'undefined' ? SockJS : require('SockJS'),
       typeof Stomp !== 'undefined' ? Stomp : require('Stomp')
-  )
+  );
   } else if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define(['angular', 'SockJS', 'Stomp'], factory)
+    define(['angular', 'SockJS', 'Stomp'], factory);
   } else {
     // Browser globals
-    factory(angular, SockJS, Stomp)
+    factory(angular, SockJS, Stomp);
   }
 }(function (angular, SockJS, Stomp) {
+  'use strict';
   angular
   .module('ngStomp', [])
   .service('$stomp', [
@@ -34,12 +34,12 @@
       // this.sock = null
       // this.debug = null
 
-      this.stomps = {}
+      this.stomps = {};
 
       function parseURI (uri) {
-        var parser = document.createElement('a')
-        parser.href = uri
-        return parser
+        var parser = document.createElement('a');
+        parser.href = uri;
+        return parser;
       }
 
       // this.setDebug = function (callback) {
@@ -47,91 +47,91 @@
       // }
 
       this.connect = function (name, endpoint, headers) {
-        headers = headers || {}
+        headers = headers || {};
 
-        var dfd = $q.defer()
+        var dfd = $q.defer();
 
         if (parseURI(endpoint).protocol === 'ws:' || parseURI(endpoint).protocol === 'wss:') {
-          var ws = new WebSocket(endpoint)
-          this.stomps[name] = Stomp.over(ws)
-          this.stomps[name].subs = {}
-          this.stomps[name].debug = null
+          var ws = new WebSocket(endpoint);
+          this.stomps[name] = Stomp.over(ws);
+          this.stomps[name].subs = {};
+          this.stomps[name].debug = null;
         } else {
-          var sock = new SockJS(endpoint)
-          this.stomps[name] = Stomp.over(sock)
-          this.stomps[name].subs = {}
-          this.stomps[name].debug = null
+          var sock = new SockJS(endpoint);
+          this.stomps[name] = Stomp.over(sock);
+          this.stomps[name].subs = {};
+          this.stomps[name].debug = null;
         }
 
         // this.stomp.debug = this.debug
         this.stomps[name].connect(headers, function (frame) {
-          dfd.resolve(frame)
+          dfd.resolve(frame);
         }, function (err) {
-          dfd.reject(err)
-        })
+          dfd.reject(err);
+        });
 
-        return dfd.promise
-      }
+        return dfd.promise;
+      };
 
       this.disconnect = function (name) {
-        var dfd = $q.defer()
+        var dfd = $q.defer();
         if (this.stomps[name] && this.stomps[name].connected) {
           for (var destination in this.stomps[name].subs)
           {
-            this.unsubscribe(name, destination)
+            if(this.stomps[name].subs.hasOwnProperty(destination)) {
+                this.unsubscribe(name, destination);
+            }
             // sub.unsubscribe()
           } 
 
-          this.stomps[name].disconnect(dfd.resolve)
+          this.stomps[name].disconnect(dfd.resolve);
         } else {
-          dfd.resolve()
+          dfd.resolve();
         }
-        return dfd.promise
-      }
+        return dfd.promise;
+      };
 
       this.subscribe = this.on = function (name, destination, headers) {
-        headers = headers || {}
+        headers = headers || {};
 
-        var dfd = $q.defer()
+        var dfd = $q.defer();
 
         if (this.stomps[name]) {
           if (this.stomps[name].connected){
             if (!this.stomps[name].subs[destination]) {
-              var sub = this.stomps[name].subscribe(destination, function (res) {
-                dfd.notify(res)
-              }, headers)
-
-              this.stomps[name].subs[destination] = sub
+                this.stomps[name].subs[destination] = this.stomps[name].subscribe(destination, function (res) {
+                  dfd.notify(res);
+              }, headers);
             }
           }
         }
 
-        return dfd.promise
-      }
+        return dfd.promise;
+      };
 
       this.unsubscribe = this.off = function (name, destination) {
         if (this.stomps[name]) {
           if (this.stomps[name].connected){
             if (this.stomps[name].subs[destination]) {
-              this.stomps[name].subs[destination].unsubscribe()
-              delete this.stomps[name].subs[destination]
+              this.stomps[name].subs[destination].unsubscribe();
+              delete this.stomps[name].subs[destination];
             }
           }
         }
-      }
+      };
 
       this.send = function (name, destination, body, headers) {
-        var dfd = $q.defer()
+        var dfd = $q.defer();
         try {
-          var payloadJson = JSON.stringify(body)
-          headers = headers || {}
-          this.stomps[name].send(destination, headers, payloadJson)
-          dfd.resolve()
+          var payloadJson = JSON.stringify(body);
+          headers = headers || {};
+          this.stomps[name].send(destination, headers, payloadJson);
+          dfd.resolve();
         } catch (e) {
-          dfd.reject(e)
+          dfd.reject(e);
         }
-        return dfd.promise
-      }
+        return dfd.promise;
+      };
     }]
-  )
-}))
+  );
+}));
